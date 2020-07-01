34ecec832bf86a3cd63ee98dbb4eab89b1fb5739
Stanley Valenzuela <s.valenzuela@limenainc.net>
2020-07-01 17:12:38 -0600

    Add project files.

---
+++ ExtensionMethods/Extensor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class Extensor
+    {
+        public static int LetterCount (this String str)
+        {
+            return str.Length;
+        }
+
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myName = "Marvin Valenzuela Contreras ?";
+            
+
+            Console.WriteLine(myName.LetterCount().ToString());
+            Console.WriteLine(myName.WordCount().ToString());
+            Console.ReadLine();
+        }
+    }
+}
